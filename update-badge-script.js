939f3931533dde20bb9acf63d2f4f55b6bbcffa8
thanusree-s-2 <thanusreesakthivel@gmail.com>
2026-07-10 15:52:54 +0530

    Update username in badge script

--- update-badge-script.js
+++ update-badge-script.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-let userName = "saran-k-07";
+let userName = "thanusree-s-2";
 
 let pipelineRun = true; // Set to false for local debugging
 let templateFile = 'github-repo-count-template.svg';
